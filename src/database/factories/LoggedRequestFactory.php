032816f05212a1b89ed6a7cd4e12804b9185b1d1
SofianeLasri <alasri250@gmail.com>
2025-02-19 16:24:11 +0100

    refactor(factory): update namespace for factory classes to follow PSR-4 standards

--- src/database/factories/LoggedRequestFactory.php
+++ src/database/factories/LoggedRequestFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SlProjects\LaravelRequestLogger\database\factories;
+namespace SlProjects\LaravelRequestLogger\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
